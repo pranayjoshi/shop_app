d50c9f14c87d5aef230eebe41a16026f54eb5f7f
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-12 22:59:50 +0530

    created

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(child: Image.network(imageUrl));
+  }
+}
