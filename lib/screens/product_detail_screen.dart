657fb9f54695c1dd9a93608d76a182518298a2fc
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-14 15:18:29 +0530

    created

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(),);
+  }
+}
