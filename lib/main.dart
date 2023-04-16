939f451304debb9acc686a3d2095113d26084d21
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-16 13:27:09 +0530

    created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/product.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 
@@ -12,16 +14,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName:(context) =>ProductDetailScreen() 
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName:(context) =>ProductDetailScreen() 
-      },
     );
   }
 }
--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/models/product.dart';
 
-class Products with ChangeNotifier {}
+class Products with ChangeNotifier {
+  List<Product> _items = [];
+
+  List<Product> get items {
+    return [...items];
+  }
+
+  void addProduct(){
+    // _items.add(value);
+    notifyListeners();
+  }
+}
