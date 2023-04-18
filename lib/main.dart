71a6d59433d6a301e2d82ee8c444c6d2d06de786
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-18 21:35:23 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Products(),
+    return ChangeNotifierProvider.value(
+      value: Products(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
