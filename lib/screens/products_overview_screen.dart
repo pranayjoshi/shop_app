3ee498c084da896f906a44a0285a09385e474593
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-20 06:58:44 +0530

    updated

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -5,22 +5,29 @@ import 'package:shop_app/models/product.dart';
 import 'package:shop_app/widgets/products_grid.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Shop"),
         actions: <Widget>[
-          PopupMenuButton(itemBuilder: (_)=>[
-            PopupMenuItem(child: Text("Only favourites"), value: 0,),
-            PopupMenuItem(child: Text("Show All"), value: 1,)
-          ] , icon: Icon(Icons.more_vert), onSelected: (value) => print(value),)
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text("Only favourites"),
+                value: 0,
+              ),
+              PopupMenuItem(
+                child: Text("Show All"),
+                value: 1,
+              )
+            ],
+            icon: Icon(Icons.more_vert),
+            onSelected: (value) => print(value),
+          )
         ],
       ),
       body: ProductsGrid(),
     );
   }
 }
-
-
