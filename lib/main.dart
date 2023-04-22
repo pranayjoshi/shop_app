7d5fec35bc4b03c4e1fda355e269ce252086ecf4
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-22 13:12:58 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/product.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
@@ -14,8 +15,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Products(),
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(
+      create: (context) => Products()),
+      ChangeNotifierProvider(
+      create: (context) => Cart()),
+    ]);
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -26,7 +31,6 @@ class MyApp extends StatelessWidget {
         routes: {
           ProductDetailScreen.routeName:(context) =>ProductDetailScreen() 
         },
-      ),
-    );
+      );
   }
 }
