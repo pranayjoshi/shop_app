bb6779471e406c1754212bb3a17027d98b6c71c8
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-23 20:27:19 +0530

    updated

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class CartScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Your Cart")),
+    );
+  }
+}
