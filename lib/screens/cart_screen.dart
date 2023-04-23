78a4bf5d9c2147d4711f039c83132b152f6124c4
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-23 20:30:48 +0530

    updated

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -7,6 +7,20 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Your Cart")),
+      body: Column(children: <Widget>[
+        Card(margin: EdgeInsets.all(15),
+          child: Padding(
+            padding: EdgeInsets.all(8),
+            child: Row(children: <Widget>[
+              Text("Total",
+                style: TextStyle(fontSize: 20)
+              ),
+              SizedBox(width: 10,),
+              Chip(label: Text(""))  
+            ],),
+            ),
+        )
+      ]),
     );
   }
 }
