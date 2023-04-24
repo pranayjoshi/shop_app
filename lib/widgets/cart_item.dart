e2676b1df28fd9ef68a5d411388d60768173927e
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-24 12:36:54 +0530

    updated

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -8,19 +8,46 @@ class CartItem extends StatelessWidget {
   final int quantity;
   final String title;
 
-  CartItem(this.id, this.title,  this.quantity, this.price,);
+  CartItem(
+    this.id,
+    this.title,
+    this.quantity,
+    this.price,
+  );
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(child: Padding(padding: EdgeInsets.all(5), child: FittedBox(child: Text('\$$price',))),),
-          title: Text(title),
-          subtitle: Text('Total: \$${price * quantity}'),
-          trailing: Text('$quantity x'),
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).colorScheme.error,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      ),
+      direction: DismissDirection.endToStart,
+      child: Card(
+        margin: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                  padding: EdgeInsets.all(5),
+                  child: FittedBox(
+                      child: Text(
+                    '\$$price',
+                  ))),
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$${price * quantity}'),
+            trailing: Text('$quantity x'),
+          ),
         ),
       ),
     );
