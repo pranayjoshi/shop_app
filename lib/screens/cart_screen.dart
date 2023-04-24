958eebb215fee99675989a6e29b57490afe46c35
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-24 06:44:14 +0530

    updated

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -5,7 +5,6 @@ import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
 
 class CartScreen extends StatelessWidget {
-
   static const routeName = "/cart";
   @override
   Widget build(BuildContext context) {
@@ -13,19 +12,32 @@ class CartScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("Your Cart")),
       body: Column(children: <Widget>[
-        Card(margin: EdgeInsets.all(15),
+        Card(
+          margin: EdgeInsets.all(15),
           child: Padding(
             padding: EdgeInsets.all(8),
-            child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween ,children: <Widget>[
-              Text("Total",
-                style: TextStyle(fontSize: 20,)
-              ),
-              SizedBox(width: 10,),
-              Spacer(),
-              Chip(label: Text('\$${cart.totalAmount}', style: TextStyle(color: Colors.white),), backgroundColor: Theme.of(context).primaryColor,),
-              OutlinedButton(onPressed: (){}, child: Text("ORDER NOW"))
-            ],),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Total",
+                    style: TextStyle(
+                      fontSize: 20,
+                    )),
+                Spacer(),
+                Chip(
+                  label: Text(
+                    '\$${cart.totalAmount}',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  backgroundColor: Theme.of(context).primaryColor,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                OutlinedButton(onPressed: () {}, child: Text("ORDER NOW"))
+              ],
             ),
+          ),
         )
       ]),
     );
