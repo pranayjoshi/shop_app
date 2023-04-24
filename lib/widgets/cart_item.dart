2083312f2dd5d55b4291b0a1321c3da5cb481812
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-24 06:51:13 +0530

    updated

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -3,10 +3,20 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class CartItem extends StatelessWidget {
-  const CartItem({super.key});
+  final String id;
+  final double price;
+  final int quantity;
+  final String title;
+
+  CartItem(this.id, this.price, this.quantity, this.title);
 
   @override
   Widget build(BuildContext context) {
-    return Card(margin: EdgeInsets.symmetric(vertical: 4, horizontal: 15),);
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 4, horizontal: 15),
+      child: ListTile(
+        leading: CircleAvatar(child: Text('\$$price')),
+      ),
+    );
   }
 }
