0e60fa6e35f683a007233bbe81e52e63d2bae2f2
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-25 20:19:00 +0530

    updated

--- lib/providers/order.dart
+++ lib/providers/order.dart
@@ -17,5 +17,9 @@ class Orders with ChangeNotifier{
     return [...orders];
   }
 
+  void addItem(List<CartItem> cartProducts, double total){
+    _orders.insert(0, OrderItem(id: DateTime.now().toString(), amount: total, products: cartProducts, dateTime: DateTime.now()));
+  }
+
   
 }
