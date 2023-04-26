3c4ee697192a20467fbbe2750ea0a4d826742c7b
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-26 16:30:39 +0530

    updated

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:provider/provider.dart';
-import 'package:shop_app/providers/order.dart' show Orders;
 import 'package:shop_app/widgets/app_drawer.dart';
-import 'package:shop_app/widgets/order_item.dart';
+
+import '../providers/order.dart' show Orders;
+import '../widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
-  
-  static const routeName = "/orders_screen";
-
+  static const routeName = '/orders';
   @override
   Widget build(BuildContext context) {
-
     final orderData = Provider.of<Orders>(context);
     return Scaffold(
-      appBar: AppBar(title: Text("Your Orders")),
-      body: ListView.builder(itemBuilder:(context, index) => OrderItem(orderData.orders[index]), itemCount: orderData.orders.length,),
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      body: ListView.builder(
+        itemCount: orderData.orders.length,
+        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+      ),
       drawer: AppDrawer(),
     );
   }
