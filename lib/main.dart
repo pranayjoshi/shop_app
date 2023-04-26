bec98d1c541c2ec2abd585ecf3f77bdb4bd92583
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-26 16:19:25 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/order.dart';
 import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/screens/orders_screen.dart';
 
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
@@ -35,7 +36,8 @@ class MyApp extends StatelessWidget {
           home: ProductsOverviewScreen(),
           routes: {
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-            CartScreen.routeName : (ctx) => CartScreen()
+            CartScreen.routeName : (ctx) => CartScreen(),
+            OrdersScreen.routeName : (ctx) => OrdersScreen()
           }),
     );
   }
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -7,6 +7,7 @@ import 'package:shop_app/widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
   
+  static const routeName = "/orders_screen";
 
   @override
   Widget build(BuildContext context) {
