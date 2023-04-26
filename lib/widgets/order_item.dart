7fa84874f48f66998fffa04e9a00cc27a0ecc69c
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-26 16:47:39 +0530

    updated

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -11,11 +11,12 @@ class OrderItem extends StatefulWidget {
   OrderItem(this.order);
 
   @override
-  State<OrderItem> createState() => _OrderItemState();
+  _OrderItemState createState() => _OrderItemState();
 }
 
 class _OrderItemState extends State<OrderItem> {
   var _expanded = false;
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -25,33 +26,47 @@ class _OrderItemState extends State<OrderItem> {
           ListTile(
             title: Text('\$${widget.order.amount}'),
             subtitle: Text(
-              DateFormat('dd MM yyyy hh:mm').format(widget.order.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
             ),
             trailing: IconButton(
               icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
               onPressed: () {
                 setState(() {
-                  _expanded:
-                  !_expanded;
+                  _expanded = !_expanded;
                 });
               },
             ),
           ),
           if (_expanded)
             Container(
-                height: min(widget.order.products.length * 20.0 + 100, 180),
-                child: ListView(
-                  children: widget.order.products.map(((e) => Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(e.title),
-                            Text(
-                              '${e.quantity}x \$${e.price}',
-                              style:
-                                  TextStyle(fontSize: 18, color: Colors.grey),
-                            )
-                          ]))) as List<Widget>,
-                ))
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(widget.order.products.length * 20.0 + 10, 100),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (prod) => Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                prod.title,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '${prod.quantity}x \$${prod.price}',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.grey,
+                                ),
+                              )
+                            ],
+                          ),
+                    )
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
