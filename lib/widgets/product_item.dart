1e31e4bd0ecdba405a9d44c730ffb8bec03f6819
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-27 07:18:49 +0530

    updated

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -54,6 +54,7 @@ class ProductItem extends StatelessWidget {
             ),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                 content: Text("Added item to the cart!"), action: SnackBarAction(label: "UNDO", onPressed: () {
                   cart.removeSingleItem(product.id);
