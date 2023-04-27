260f565a7096c7d8add333327e025558c946d65b
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-27 11:51:24 +0530

    updated

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class EditProductScreen extends StatefulWidget {
-
   @override
   State<EditProductScreen> createState() => _EditProductScreenState();
 }
@@ -13,9 +12,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Edit Produc")),
-      body: Form(child: ListView(children: <Widget>[
-        TextFormField()
-      ],)),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Form(
+            child: ListView(
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(labelText: "Title"),
+              textInputAction: TextInputAction.next,
+            )
+          ],
+        )),
+      ),
     );
   }
 }
