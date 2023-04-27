86596cd8ce2dbfb31fa287aefcb46b66a19c9ae4
pranayjoshi <pranayjoshi4466@gmail.com>
2023-04-27 11:26:12 +0530

    updated

--- lib/screens/user_product_screen.dart
+++ lib/screens/user_product_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/product.dart';
+import 'package:shop_app/widgets/user_product_item.dart';
 
 class UserProductScreen extends StatelessWidget {
   const UserProductScreen({super.key});
@@ -15,7 +16,16 @@ class UserProductScreen extends StatelessWidget {
         title: Text("Your Product"),
         actions: <Widget>[IconButton(onPressed: () {}, icon: Icon(Icons.add))],
       ),
-      body: Padding(padding: EdgeInsets.all(8), child: ListView.builder(itemBuilder: (count, i) {}, itemCount: productsData.items.length,),),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemBuilder: (count, i) {
+            UserProductItem(
+                productsData.items[i].title, productsData.items[i].imageUrl);
+          },
+          itemCount: productsData.items.length,
+        ),
+      ),
     );
   }
 }
