5bf93b98a35b6ad287c5a89b4f0dee9bf499861b
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-03 22:07:23 +0530

    updated

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http; 
+import 'package:http/http.dart' as http;
 
 import '../models/product.dart';
 
@@ -68,24 +68,32 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
-    final url = Uri.https('flutter-test1-e3bd1-default-rtdb.asia-southeast1.firebasedatabase.app', '/products.json');
-    http.post(url, body: json.encode({
-      'title': product.title,
-      'description': product.description,
-      'imageUrl': product.imageUrl,
-      'price': product.price,
-      'isFavourite': product.isFavourite,
-    }),);
-    final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      id: DateTime.now().toString(),
-    );
-    _items.add(newProduct);
-    // _items.insert(0, newProduct); // at the start of the list
-    notifyListeners();
+    final url = Uri.https(
+        'flutter-test1-e3bd1-default-rtdb.asia-southeast1.firebasedatabase.app',
+        '/products.json');
+    http
+        .post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavourite': product.isFavourite,
+      }),
+    )
+        .then((value) {
+      final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id: DateTime.now().toString(),
+      );
+      _items.add(newProduct);
+      // _items.insert(0, newProduct); // at the start of the list
+      notifyListeners();
+    });
   }
 
   void updateProduct(String id, Product newProduct) {
