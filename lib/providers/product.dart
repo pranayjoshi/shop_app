f365a0f07c48174d78f0694f7d2aa475aea5691d
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-04 07:54:25 +0530

    updated

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -122,9 +122,19 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url = Uri.https(
+        'flutter-test1-e3bd1-default-rtdb.asia-southeast1.firebasedatabase.app',
+        '/products/${id}.json');
+      await http.patch(url, body: json.encode({
+          'title': newProduct.title,
+          'description': newProduct.description,
+          'imageUrl': newProduct.imageUrl,
+          'price': newProduct.price,
+        }),);
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
