5d39f4e7b4a490de32c1a650dccbd9f0de3b3d6c
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-04 07:32:48 +0530

    updated

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -20,13 +20,14 @@ class ProductsOverviewScreen extends StatefulWidget {
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavorites = false;
-  var _isInit = false;
+  var _isInit = true;
 
   @override
   void didChangeDependencies() {
     if (_isInit){
       Provider.of<Products>(context).fetchAndSetProducts();
     }
+    _isInit = false;
     super.didChangeDependencies();
   }
 
