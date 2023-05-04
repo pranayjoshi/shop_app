20ee1fff0e40abd114de965b4416f45f53f375f7
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-04 10:25:57 +0530

    updated

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http ;
 
 class Product with ChangeNotifier{
   final String id;
@@ -11,11 +14,20 @@ class Product with ChangeNotifier{
 
   Product({required this.id, required this.title, required this.price, required this.description, required this.imageUrl, this.isFavourite = false});
 
-  void toggleFavourite(){
+  Future<void> toggleFavourite() async{
+    final url = Uri.https(
+      'flutter-test1-e3bd1-default-rtdb.asia-southeast1.firebasedatabase.app',
+      '/products/${id}.json');
+
+    var extfavind = isFavourite;
     isFavourite = !isFavourite;
     notifyListeners();
+    try {
+      final res = await http.patch(url,body: json.encode({'isFavourite': isFavourite}));
+    } catch(err){
+      
+    }
+    
   }
-
-  void toggleFavoriteStatus() {}
 
 }
