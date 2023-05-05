789b34ded8ccf338d17a478d6282b57b1535549c
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-05 15:55:09 +0530

    updated

--- lib/providers/order.dart
+++ lib/providers/order.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 import './cart.dart';
 
@@ -23,13 +26,29 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  void addOrder(List<CartItem> cartProducts, double total) async{
+    final url = Uri.https(
+      'flutter-test1-e3bd1-default-rtdb.asia-southeast1.firebasedatabase.app',
+      '/products.json');
+
+    final timestamp = DateTime.now();
+
+    http.post(url, body: json.encode({
+      'amount': total,
+      'dateTime': timestamp.toIso8601String(),
+      'prodcus': cartProducts.map((e) =>{
+        'id': e.id,
+        'title': e.title,
+        'quantity': e.quantity,
+        'price':e.price,
+      })
+    }));
     _orders.insert(
       0,
       OrderItem(
         id: DateTime.now().toString(),
         amount: total,
-        dateTime: DateTime.now(),
+        dateTime: timestamp,
         products: cartProducts,
       ),
     );
