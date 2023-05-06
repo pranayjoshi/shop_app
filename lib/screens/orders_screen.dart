0953d46d62758ccdc675140d2028933bd0db3137
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-06 15:40:09 +0530

    updated

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -14,10 +14,18 @@ class OrdersScreen extends StatefulWidget {
 
 class _OrdersScreenState extends State<OrdersScreen> {
 
+  var _isLoading = false;
+
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) {
-      Provider.of<Orders>(context, listen: false).fetchAndSetProducts();
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<Orders>(context, listen: false).fetchAndSetProducts();
+      setState(() {
+        _isLoading = false;
+      });
     });
     super.initState();
   }
@@ -29,7 +37,7 @@ class _OrdersScreenState extends State<OrdersScreen> {
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
+      body: _isLoading ? Center(child: CircularProgressIndicator()) : ListView.builder(
         itemCount: orderData.orders.length,
         itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
       ),
