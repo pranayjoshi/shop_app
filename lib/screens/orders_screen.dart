0140cda2d9cc12e7d3c667990a19cd97f1b3dc9d
pranayjoshi <pranayjoshi4466@gmail.com>
2023-05-06 13:19:53 +0530

    updated

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,9 +5,22 @@ import '../providers/order.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
 
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) {
+      Provider.of<Orders>(context).fetchAndSetProducts();
+    });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
